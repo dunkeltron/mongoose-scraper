4a223e98f973e908783f1e2e2f700faff1fabfad
Ian Dunkelberg <idunkelberg@gmail.com>
2018-12-21 14:04:04 -0800

    scraping from reddit and displaying to site. front end is ugly but serviceable. Need to work on mongo and displaying comments.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,19 +7,17 @@ module.exports = function (app, axios, cheerio) {
     //default front page of reddit scrape
     //Look into expanding to allow for scraping of any reddit page  via req.params
     app.get("/scrape", function (req, res) {
+        var articlesList =[];
         // First, we grab the body of the html with axios
         axios.get("http://www.reddit.com/").then(function (response) {
             // Then, we load that into cheerio and save it to $ for a shorthand selector
             var $ = cheerio.load(response.data);
 
             // Now, we grab every h2 within an article tag, and do the following:
-            $("._1poyrkZ7g36PawDueRza-J").each(function (i, element) {
-                if(i==0){
-                console.log($(this).children("article"));
-                i++;e
-                }
-                if ($(this).children("article")._root[0].name == "div") {
-                    // Save an empty result object
+            $("._1poyrkZ7g36PawDueRza-J ").each(function (i, element) {
+                var numOfClasses = $(this)[0].attribs.class.split(" ").length;
+                if (numOfClasses === 2) {
+                    //Save an empty result object
                     var result = {};
                     var topBarInfo = $(this).children(".s1ssr92a-0")
                         .children(".cZPZhMe-UCZ8htPodMyJ5")
@@ -29,14 +27,19 @@ module.exports = function (app, axios, cheerio) {
                     result.title = titlePlusHref
                         .children("h2")
                         .text();
-                    result.link = titlePlusHref
+                    result.link = "http://www.reddit.com"+titlePlusHref
                         .attr("href");
 
 
                     result.subreddit = topBarInfo.children(".bsfRLa").attr("href");
 
                     result.postAge = topBarInfo.children("._3jOxDPIQ0KaOWpzvSQo-1s").text();
-                    // Create a new Article using the `result` object built from scraping
+
+                    if (result.title == '' || result.link == undefined || result.subreddit == undefined || result.postAge == '') {
+
+                    } else {
+                        articlesList.push(result);
+                        // Create a new Article using the `result` object built from scraping
                     // db.Post.create(result)
                     //   .then(function(dbPost) {
                     //     // View the added result in the console
@@ -46,13 +49,9 @@ module.exports = function (app, axios, cheerio) {
                     //     // If an error occurred, log it
                     //     console.log(err);
                     //   });
-                    if (result.title == '' || result.link == undefined || result.subreddit == undefined || result.postAge == '') {
-
-                    } else {
-                        //console.log(result);
                     }
                 }
-                else if ($(this).children("article")._root[0].name == "article"){
+                else if (numOfClasses === 3){
                     var result = {};
                     
                     var topBarInfo = $(this).children(".fmYTpB").children(".ddpzfR").children(".s1ssr92a-0")
@@ -63,7 +62,7 @@ module.exports = function (app, axios, cheerio) {
                     result.title = titlePlusHref
                         .children("h2")
                         .text();
-                    result.link = titlePlusHref
+                        result.link = "http://www.reddit.com"+titlePlusHref
                         .attr("href");
 
 
@@ -73,15 +72,28 @@ module.exports = function (app, axios, cheerio) {
                     if (result.title == '' || result.link == undefined || result.subreddit == undefined || result.postAge == '') {
 
                     } else {
-                        //console.log(result);
+                        articlesList.push(result);
+                        // Create a new Article using the `result` object built from scraping
+                    // db.Post.create(result)
+                    //   .then(function(dbPost) {
+                    //     // View the added result in the console
+                    //     console.log(dbPost);
+                    //   })
+                    //   .catch(function(err) {
+                    //     // If an error occurred, log it
+                    //     console.log(err);
+                    //   });
                     }
                 }
                 
 
-            });
-
+            })
+            
             // Send a message to the client
-            res.send("Scrape Complete");
+            var hbsObject = {
+                articles: articlesList
+            };
+            res.render("index",hbsObject);
         });
     });
 }
